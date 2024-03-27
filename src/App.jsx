4ed64a216a65086e5729dc37f33856b40673cc3a
Vansh Sardana <vanshsrdn25703@gmail.com>
2024-03-27 23:17:30 +0530

    adding calendar

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,12 @@
-import React, { useEffect, useState } from 'react'
-
+import React from 'react'
+import Clock from './components/Clock'
+import Calendar from './components/Calendar'
 
 const App = () => {
-  const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
-  const months= ["January","February","March","April","May","June","July","August","September","October","November","December"];
-  const [cHour, setcHour]= useState(0);
-  const [cMin, setcMin]= useState(0);
-  const [cSec, setcSec]= useState(0);
-  const [cDay, setcDay]= useState(0);
-  const [cDate, setcDate]= useState(0);
-  const [cYear, setcYear]= useState(0);
-  const [cMonth, setcMonth]= useState(0);
-
-
-  function getTime(){
-      setInterval(() => {
-        const time= new Date();
-        
-        const hour= time.getHours();
-        hour<10? setcHour('0'+hour):setcHour(hour);
-      
-        const min= (time.getMinutes());
-        (min<10)? setcMin('0'+min):setcMin(min);
-        
-        const sec= time.getSeconds();
-        sec<10? setcSec('0'+sec): setcSec(sec);
-      
-        setcDay(time.getDay());
-
-        const date= time.getDate();
-        date<10? setcDate('0'+date): setcDate(date);
-
-        setcYear(time.getFullYear());
-        setcMonth(time.getMonth());
-
-        
-    }, 1000);
-  }
-
-  useEffect(()=>{
-    getTime();
-  }, []);
-
   return (
-    <div className=' text-[4rem] bg-[rgb(183,158,158)] min-h-screen w-screen text-white flex justify-center items-center font-[900] relative flex-col p-10'>
-      <div className='text-[1.2rem] md:text-[2rem] gap-[8px] flex justify-around flex-wrap'>
-        {
-          daysOfWeek.map((day, index)=>{
-            return <div className={index==cDay?"text-white": "text-[rgb(228,204,204)]"}>{day}</div>
-          })
-        }
-      </div>
-
-      <div className='flex justify-center items-center md:text-[6.25rem] '>
-        <span> {cHour} </span>
-        <span> : </span>
-        <span> {cMin} </span>
-        <span> : </span>
-        <span> {cSec} </span>
-      </div>
-
-      <div className='flex justify-center items-center text-[1.8rem] md:text-[2.5rem]'>
-          <span>{cDate}</span>
-          <pre>, </pre>
-          <span>{months[cMonth]}</span>
-          <pre> </pre>
-          <span>{cYear}</span>
-
-      </div>
+    <div className=' text-[4rem] bg-[rgb(183,158,158)] min-h-screen w-screen relative text-white font-[900] flex flex-col justify-center items-center p-10'>
+      <Clock/>
+      <Calendar/>
     </div>
   )
 }
---
+++ src/components/Calendar.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react'
+
+const Calendar = () => {
+    const w = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+    const [currDay, setcurrDay]= useState(0);
+    const [currDate, setcurrDate]= useState(0);
+    const [currYear, setcurrYear]= useState(0);
+    const [currMonth, setcurrMonth]= useState(0);
+    function isLeap(y){
+        return (currYear % 4 === 0 && currYear % 100 !== 0) || (currYear % 400 === 0);
+    }
+    const daysOfMonth= [31,28,31,30,31,30,31,31,30,31,30,31];
+    isLeap(currYear)?daysOfMonth[1]=29:28;
+    let numbersArray = [];
+    const day1= new Date(currYear, currMonth, 1).getDay();
+    for(let i=0; i<day1; i++){
+        numbersArray.push("");
+    }
+    for(let i=1; i<=daysOfMonth[currMonth]; i++){
+        numbersArray.push(i);
+    }
+    function setDate(){
+        const date= new Date();
+        setcurrDate(date.getDate());
+        setcurrMonth(date.getMonth());
+        setcurrYear(date.getFullYear());
+    }
+    useEffect(()=>{
+        setDate()
+    },[]);
+  return (
+    <div className='mt-20'>
+        <div className='grid grid-cols-7 gap-x-4 text-[1.5rem]'>
+        {
+          w.map((day, index)=>{
+            return <div className='flex justify-center items-center'>{day}</div>
+          })
+        }
+        </div>
+        <div className='grid grid-cols-7 gap-x-4 text-[1.5rem]'>
+
+            {
+                numbersArray.map((d)=>{
+                    return <div className={`${currDate==d?"text-white":"text-[rgb(228,204,204)]"} flex justify-center items-center`}>{d}</div>
+                })
+            }
+        </div>
+    </div>
+  )
+}
+
+export default Calendar
